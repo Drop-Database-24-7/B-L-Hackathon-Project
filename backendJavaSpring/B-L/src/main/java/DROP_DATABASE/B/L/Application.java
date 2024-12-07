e4b3f1d13ecbf146e3bf061fdd6d858a77060c27
opax <opaaaax@gmail.com>
2024-12-07 13:41:25 +0100

    initialized spring

---
+++ backendJavaSpring/B-L/src/main/java/DROP_DATABASE/B/L/Application.java
@@ -0,0 +1,13 @@
+package DROP_DATABASE.B.L;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
