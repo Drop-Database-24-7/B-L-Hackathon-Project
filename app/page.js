545b1448bc76e76d876ad7b68993db03b4b76e98
Gabryslat5 <damian@jegojego.pl>
2024-12-07 15:30:39 +0100

    chooser v1

--- app/page.js
+++ app/page.js
@@ -1,9 +1,35 @@
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Combobox } from "@/components/ui/Combobox";
+
+
 
 export default function Home() {
+
+
   return (
     <div>
-      
+      <Card>
+  <CardHeader>
+    <CardTitle>Satelite Chooser</CardTitle>
+    {/* <CardDescription>Card Description</CardDescription> */}
+  </CardHeader>
+  <CardContent>
+    {/* <p>Card Content</p> */}
+    <Combobox />
+  </CardContent>
+  <CardFooter>
+    {/* <p>Card Footer</p> */}
+  </CardFooter>
+</Card>
+    {/* <Combobox /> */}
     </div>
   );
 }
