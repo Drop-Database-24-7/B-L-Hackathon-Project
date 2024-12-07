ddcb7fa830496c44615522ec17e64031689e8fe2
granek-ux <kuba.balina@gmail.com>
2024-12-07 13:57:24 +0100

    SateliteMath
    Dodano klase SateliteMath i w niej metode Distance

---
+++ backendJavaSpring/B-L/src/main/java/Utility/SateliteMath.java
@@ -0,0 +1,10 @@
+public class SateliteMath {
+
+    public static double Distance(Point p1, Point p2) {
+        double dx = p2.getX() - p1.getX();
+        double dy = p2.getY() - p1.getY();
+        double answer;
+        answer = Math.sqrt(dx * dx + dy * dy);
+        return answer;
+    }
+}
